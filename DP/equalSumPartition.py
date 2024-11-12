3f0f13bb05bdfa77aadfa57862015b97f7a019ba
Srushti Sangawar <srushtisangawar@Srushtis-MacBook-Air.local>
2024-11-12 16:21:36 -0700

    DSA: DP Basics

--- DP/equalSumPartition.py
+++ DP/equalSumPartition.py
@@ -23,8 +23,25 @@ def isSubsetSum(arr, x):
 
     return dp[n][x]
 
+def countNoOfSubsetsWithGivenSum(arr, target):
+    n = len(arr)
+    if n == 0 or target == 0: return 1
+    dp = [[0]*(target+1) for _ in range(n+1)]
+    for i in range(n+1):
+        dp[i][0] = 1
+    for i in range(1, n+1):
+        for j in range(1, target+1):
+            if j >= arr[i-1]:
+                dp[i][j] = dp[i-1][j] + dp[i-1][j-arr[i-1]]
+            else:
+                dp[i][j] = dp[i-1][j]
+    return dp[n][target]
+
 arr1 = [1,2,3003,4]
 print(equalSumpartitionProblem(arr1, sum(arr1)))
 
 arr2 = [1,5,5,11]
 print(equalSumpartitionProblem(arr2, sum(arr2)))
+
+arr3 = [2,3,5,6,8,10]
+print(countNoOfSubsetsWithGivenSum(arr3, 10))
